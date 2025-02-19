d2d2627a8df43866b77d4a4a6b69b0f162c8b283
Fahrizarizky <fahrizarizky6@gmail.com>
2025-02-20 04:48:53 +0700

    day 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,14 @@
-import { useCallback, useMemo, useRef, useState } from "react";
-import Card from "./components/Card";
-import { CardCollections } from "./utils/utils";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import MemoryGame from "./MemoryGame";
 
 function App() {
-  const myName = useMemo(() => "Arsadi", []);
-  const calculateNumber = useCallback(() => {
-    console.log("calculate number!!!");
-  }, []);
-
-  const updateCount = useRef<number>(0);
-  const [update, setUpdate] = useState<number>(0);
-  console.log({ updateCount });
 
   return (
-    <div className="">
-      <h1
-        onClick={() => {
-          alert("update count " + updateCount.current);
-        }}
-        className="text-6xl"
-      >
-        Hello {myName}
-      </h1>
-      {CardCollections.map((card) => {
-        return <Card image={card.image} />;
-      })}
-      <button
-        onClick={() => {
-          // setUpdate(update + 1);
-          // const newUpdate = update + 1;
-
-          updateCount.current += 1;
-          setUpdate(update + 1);
-          calculateNumber();
-
-          // setUpdate(newUpdate);
-          // setNumberOfClick(newUpdate + 1);
-          // console.log('click ', {newUpdate});
-        }}
-      >
-        Click
-      </button>
+    <div className="min-h-screen flex flex-col justify-between">
+      <Header />
+      <MemoryGame />
+      <Footer />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
-// import Astro from "../assets/cards/astro.jpg";
 import BackCard from "../assets/cards/back-cover-2.jpg";
 
-const Card: React.FC<{image: string}> = ({image}) => {
-  const [open, setOpen] = useState<boolean>(false);
+interface CardProps {
+  image: string;
+  key: string;
+  open: boolean;
+  onClick: () => void;
+}
 
-  // Arbitrary values;
+const Card: React.FC<CardProps> = ({image,key, open, onClick}) => {
+
   return (
     <div
-      onClick={() => setOpen(!open)}
+      onClick={onClick}
+      key={key}
       className={`relative w-24 aspect-[3/4] ${
         open ? "[transform:rotateY(180deg)]" : "hover:scale-105"
-      } [transform-style:preserve-3d] transition-all duration-300 cursor-pointer`}
+      } [transform-style:preserve-3d] bg-red-400 transition-all duration-300 cursor-pointer`}
     >
       <div className=" w-full h-full bg-blue-300 absolute [backface-visibility:hidden] ">
         <img src={BackCard} alt="" />
